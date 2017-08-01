33df2ca05bb28ba79241b362fee7a54cbff73d60
Ray Santiago <RSantiago1186@gmail.com>
2017-07-31 21:56:14 -0700

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  # config.mailer_sender = 'dan.rice.92@outlook.com'
+  # config.mailer_sender = 'RSantiago1186@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "MyMailer"
